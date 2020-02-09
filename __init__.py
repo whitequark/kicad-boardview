3d6a1bd0a3fe63677354411050013e57634039b9
Grzegorz Szymaszek <gszymaszek@short.pl>
2020-02-09 19:40:25 +0100

    Fix relative import in __init__.py
    This commit makes it possible to actually use the plugin as a Python
    module. Previously, Python would try to find pcbnew2boardview in the
    globally installed modules instead of using the local
    pcbnew2boardview.py file.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import pcbnew
-import pcbnew2boardview
 import os
+
+from .pcbnew2boardview import convert
+
 
 class Pcbnew2Boardview(pcbnew.ActionPlugin):
 
@@ -12,7 +14,7 @@ class Pcbnew2Boardview(pcbnew.ActionPlugin):
     def Run(self):
         kicad_pcb = pcbnew.GetBoard()
         with open(kicad_pcb.GetFileName().replace('.kicad_pcb', '.brd'), 'wt') as brd_file:
-            pcbnew2boardview.convert(kicad_pcb, brd_file)
+            convert(kicad_pcb, brd_file)
 
 
 plugin = Pcbnew2Boardview()
